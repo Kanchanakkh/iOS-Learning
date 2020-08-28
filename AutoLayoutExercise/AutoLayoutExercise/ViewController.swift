0752456c2c473a9e81bafdc3239c3f2d146c8519
Kanchanakkh <kanchanakkh@gmail.com>
2020-08-28 08:53:59 -0700

    upload all the files and folders for iOS

---
+++ AutoLayoutExercise/AutoLayoutExercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayoutExercise
+//
+//  Created by Kanchanak Khat on 8/22/20.
+//  Copyright © 2020 Kanchanak Khat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
