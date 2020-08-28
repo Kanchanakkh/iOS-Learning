3a40f161d3cf82eb5d93ab3b2696f96bf649b5e2
Kanchanakkh <kanchanakkh@gmail.com>
2020-08-28 08:54:47 -0700

    add a new folder

---
+++ AutoLayout2Exercise/AutoLayout2Exercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayout2Exercise
+//
+//  Created by Kanchanak Khat on 8/23/20.
+//  Copyright © 2020 Kanchanak Khat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
